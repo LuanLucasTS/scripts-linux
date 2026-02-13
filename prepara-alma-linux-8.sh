940432194179246f410979be379a36d5e5e81fe0
Luan Lucas <54227604+LuanLucasTS@users.noreply.github.com>
2026-02-13 06:43:53 -0400

    Update timezone and add ncdu to installation script
    Replaced 'America/Campo_Grande' with 'SEU_TIMEZONE' for timezone setting and added 'ncdu' to the list of installed packages.

--- prepara-alma-linux-8.sh
+++ prepara-alma-linux-8.sh
@@ -2,9 +2,9 @@ dnf install -y epel-release
 dnf install -y yum-utils
 dnf-config-manager --setopt=fastestmirror=True --save
 dnf update -y
-sudo dnf install -y nano vim wget curl git htop unzip zip bash-completion net-tools bind-utils  traceroute rsync chrony
+sudo dnf install -y nano vim wget curl git htop unzip zip bash-completion net-tools bind-utils traceroute rsync chrony ncdu
 systemctl enable --now chronyd
-timedatectl set-timezone America/Campo_Grande
+timedatectl set-timezone "SEU_TIMEZONE"
 sudo dnf install -y fail2ban
 sudo systemctl enable --now fail2ban
 reboot
