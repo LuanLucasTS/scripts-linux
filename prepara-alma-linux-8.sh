5073bf107ad096fd327f81dc73214b50dfce2aff
Luan Lucas <54227604+LuanLucasTS@users.noreply.github.com>
2026-02-13 06:58:05 -0400

    Format shell script for consistency

--- prepara-alma-linux-8.sh
+++ prepara-alma-linux-8.sh
@@ -2,9 +2,10 @@ dnf install -y epel-release
 dnf install -y yum-utils
 dnf-config-manager --setopt=fastestmirror=True --save
 dnf update -y
-sudo dnf install -y nano vim wget curl git htop unzip zip bash-completion net-tools bind-utils traceroute rsync chrony ncdu
+dnf install -y nano vim wget curl git htop unzip zip bash-completion net-tools bind-utils traceroute rsync chrony ncdu
 systemctl enable --now chronyd
 timedatectl set-timezone "SEU_TIMEZONE"
 sudo dnf install -y fail2ban
 sudo systemctl enable --now fail2ban
 reboot
+
