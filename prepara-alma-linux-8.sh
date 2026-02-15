271098778b80c6d9202fade5ed3a8c4caab7bfd6
Luan Lucas <54227604+LuanLucasTS@users.noreply.github.com>
2026-02-15 02:11:23 -0400

    Refactor shell script for Alma Linux setup

--- prepara-alma-linux-8.sh
+++ prepara-alma-linux-8.sh
@@ -1,5 +1,4 @@
-dnf install -y epel-release
-dnf install -y yum-utils
+dnf install -y epel-release yum-utils
 dnf-config-manager --setopt=fastestmirror=True --save
 dnf update -y
 dnf install -y nano vim wget curl git htop unzip zip bash-completion net-tools bind-utils traceroute rsync chrony ncdu
@@ -8,4 +7,5 @@ timedatectl set-timezone "SEU_TIMEZONE"
 sudo dnf install -y fail2ban
 sudo systemctl enable --now fail2ban
 reboot
+
 
